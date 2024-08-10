48e96204e596f1fc4887979499f2379225945c6b
Publius8 <javadtariverdiyev@gmail.com>
2024-08-11 01:44:40 +0400

    favicon and title

--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-import Navbar from '../src/assets/components/navbar.jsx'
-import Footer from '../src/assets/components/Footer.jsx'
-import './assets/css/index.css'
-import HomePage from "./assets/components/homePage.jsx"
-import ContactPage from "./assets/components/ContactPage.jsx"
-import BlogPage from "./assets/components/BlogPage.jsx"
-import HotelPage from "./assets/components/HotelPage.jsx"
-import AboutUsPage from "./assets/components/AboutUsPage.jsx"
-import PlacePage from "./assets/components/PlacePage.jsx"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Navbar from '../src/assets/components/navbar.jsx';
+import Footer from '../src/assets/components/Footer.jsx';
+import './assets/css/index.css';
+
+import HomePage from "./assets/components/HomePage.jsx";
+import ContactPage from "./assets/components/ContactPage.jsx";
+import BlogPage from "./assets/components/BlogPage.jsx";
+import HotelPage from "./assets/components/HotelPage.jsx";
+import AboutUsPage from "./assets/components/AboutUsPage.jsx";
+import PlacePage from "./assets/components/PlacePage.jsx";
 
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
-]);
+function App() {
+  return (
+    <StrictMode>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/places" element={<PlacePage />} />
+          <Route path="/hotels" element={<HotelPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </StrictMode>
+  );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Navbar />
-    {/* <HomePage /> */}
-    {/* <AboutUsPage /> */}
-    {/* <PlacePage /> */}
-    {/* <HotelPage /> */}
-    {/* <BlogPage /> */}
-    <ContactPage/>
-    <Footer />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />);
